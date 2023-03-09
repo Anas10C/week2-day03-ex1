caafabb50660621ce2b86d4e4149095cfe970690
Anas10C <firarianas@gmail.com>
2023-03-09 18:09:38 +0100

    Annotation DI

--- src/main/java/org/example/HelloWorldDecoupled.java
+++ src/main/java/org/example/HelloWorldDecoupled.java
@@ -1,11 +1,11 @@
 package org.example;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class HelloWorldDecoupled {
     public static void main(String... args) {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("/app-context.xml");
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(HelloWorldClassConfiguration.class);
         MessageRenderer mr = ctx.getBean("renderer", MessageRenderer.class);
         mr.render();
     }
